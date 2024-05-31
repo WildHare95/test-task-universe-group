dfc965f98a7b7acdca0196604ccb10712ce4816d
Denis Tsykalov <denis.tsykalov@chisw.com>
2024-05-31 23:54:20 +0200

    markup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {initFlowbite} from "flowbite";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-task-universe-group';
+
+  history = [1,2,3,4,5,6,7,8,9]
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
